2f1abd36d28d6449eb0f009ca62e1ba98f3d6602
Piotrkn <piotrknop7@gmail.com>
2015-05-07 17:32:11 +0200

    Liczenie macierzy

--- SMiP/src/NetProperties/NetProperties.java
+++ SMiP/src/NetProperties/NetProperties.java
@@ -5,9 +5,12 @@
  */
 package NetProperties;
 
-import java.util.Collection;
 import model.node;
 import smip.SMiP;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class NetProperties {
     public boolean netIsAlive = true;
@@ -33,6 +36,51 @@ public class NetProperties {
             }
         }
         return netIsAlive;
+    }
+
+    public int[][] getIncidenceMatrix() {
+        Collection<node> nodes = SMiP.graphNet.getVertices();
+        List<node> transitions = new ArrayList<>();
+        List<node> places = new ArrayList<>();
+        int[][] inputsMatrix;
+        int[][] outputsMatrix;
+        int[][] incidenceMatrix;
+
+        for (node node : nodes) {
+            if (node.isTransition()) {
+                transitions.add(node);
+            } else {
+                places.add(node);
+            }
+        }
+
+        inputsMatrix = new int[transitions.size()][places.size()];
+        outputsMatrix = new int[transitions.size()][places.size()];
+        incidenceMatrix = new int[transitions.size()][places.size()];
+
+        for (int i = 0; i < transitions.size(); i++) {
+            node transition = transitions.get(i);
+
+            for (int j = 0; j < places.size(); j++) {
+                node place = places.get(j);
+
+                if (SMiP.graphNet.isPredecessor(transition, place)) {
+                    inputsMatrix[i][j] = 1;
+                } else {
+                    inputsMatrix[i][j] = 0;
+                }
+
+                if (SMiP.graphNet.isSuccessor(transition, place)) {
+                    outputsMatrix[i][j] = 1;
+                } else {
+                    outputsMatrix[i][j] = 0;
+                }
+
+                incidenceMatrix[i][j] = inputsMatrix[i][j] - outputsMatrix[i][j];
+            }
+        }
+
+        return incidenceMatrix;
     }
     
     public void resetProperties()
